ef358a8b41b0fe89e9629d50fef0d9069504f4e3
Andrew Goldstone <andrew.goldstone@gmail.com>
2015-11-23 10:00:35 -0500

    dashfree pkg rename

--- R/ragoldstBeamer-package.R
+++ R/ragoldstBeamer-package.R
@@ -1,3 +1,10 @@
+#' Miscellaneous R Utilities for Beamer Presentations
+#'
+#' This package includes some utilities for styling and laying out beamer presentations.
+#'
+#' @name ragoldstBeamer
+#' @docType package
+NULL
 
 ragoldst_local <- new.env(parent=emptyenv())
 
