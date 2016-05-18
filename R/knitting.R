c5dbce02b823223ceb0c2b84bcc47e7da84381e7
Andrew Goldstone <andrew.goldstone@gmail.com>
2016-05-18 15:33:32 -0400

    plot_font chunk option processing

--- R/knitting.R
+++ R/knitting.R
@@ -1,12 +1,14 @@
 #' Code output environment hook
-#' 
-#' Not called directly but automatically set as knitr's output hook. This puts R
-#' output into an \code{ROutput} environment which it is up to you to define
-#' with something like 
-#' \code{\\DefineVerbatimEnvironment{ROutput}{Verbatim}{frame=single,fontsize=\\footnotesize}} (which is what's used in the included \code{talk} RMarkdown template).
-#' 
+#'
+#' Not called directly but automatically set as knitr's output hook. This puts
+#' R output into an \code{ROutput} environment which it is up to you to define
+#' with something like
+#' \code{\\DefineVerbatimEnvironment{ROutput}{Verbatim}{frame=single,fontsize=\\footnotesize}}
+#' (which is what's in the included pandoc templates used with the
+#' \code{lectures} R Markdown template).
+#'
 #' @export
-#' 
+#'
 output_hook_routput <- function (x, options) {
     paste(c("\\begin{ROutput}",
             sub("\n$", "", x),
@@ -16,52 +18,52 @@ output_hook_routput <- function (x, options) {
 }
 
 #' Plot placement and resizing hook
-#' 
+#'
 #' Not called directly but automatically set as knitr's plot hook, so that
 #' chunks with plots can process the chunk options \code{textblock_width} and
 #' \code{inside_textblock}, which allow you to use the \code{textpos} grid for
 #' placing and sizing plots on slides arbitrarily instead of in the flow of
 #' text.
-#' 
+#'
 #' This gives us some new options and changes the meaning of two others:
-#' 
+#'
 #' \describe{
-#' 
+#'
 #' \item{\code{out.width, out.height}}{reinterpreted as units on the textpos
 #' grid. This sets the scaling by pdflatex of an image file whose generated
 #' dimensions are set by \code{fig.width} and \code{fig.height} (which are
 #' always in inches).}
-#' 
+#'
 #' \item{\code{textblock_width}}{width of the block in \code{textpos} grid
 #' units}
-#' 
+#'
 #' \item{\code{textblock_pos}}{two-element vector of grid coordinates of the
 #' textblock}
-#' 
+#'
 #' \item{\code{center}}{Boolean: enclose graphic within \code{center}
 #' environment?}
-#' 
+#'
 #' \item{\code{inside_textblock}}{don't generate \code{textblock} environment,
 #' just the \code{\\includegraphics} line (useful if, e.g., you write out the
 #' environment \code{\\begin} and \code{\\end} yourself and stick the code chunk
 #' between them)}
-#' 
+#'
 #' }
-#' 
+#'
 #' Grid positioning only happens if \code{inside_textblock=T} or
 #' \code{textblock_width} is specified in the chunk positions; otherwise we
 #' revert to knitr's normal behavior. Grid positioning also always makes
 #' graphics inline rather than floating. Use \code{inside_textblock=T} and
 #' enclose it in a \code{figure} environment yourself if you want to combine the
 #' textblock and the figure environment (you probably don't).
-#' 
+#'
 #' N.B. the default knitr plot output hook for LaTeX responds to many more
 #' options than this does. But in my workflow we are knitting to markdown, not
 #' LaTeX. That's a simplifying decision in some ways but with some costs in
 #' flexibility.
-#' 
+#'
 #' @export
-#' 
+#'
 plot_hook_textpos <- function (x, options) {
     inside <- options$inside_textblock
     pos <- options$textblock_pos
@@ -113,20 +115,28 @@ plot_hook_textpos <- function (x, options) {
 
 #' TikZ and XeLaTeX wrangling hook
 #'
-#' This hook ensures that the \pkg{tikzDevice} package options are
-#' correctly set to use xelatex and the document main font to generate
-#' graphics. It is set as the `tikz_xelatex` hook and by default all
-#' chunks have `tikz_xelatex=T` among their options.
+#' This hook ensures that the \pkg{tikzDevice} package options are correctly
+#' set to use xelatex and the chosen font (chunk option \code{plotfont},
+#' normally specified by the document metadata either explicitly or implicitly)
+#' to generate graphics. It is set as the \code{tikz_xelatex} hook and by
+#' default all chunks have \code{tikz_xelatex=T} among their options.
 #'
-#' @export 
+#' @export
 tikz_setup_hook <- function (before, options, envir) {
     if (before) {
         options(tikzDefaultEngine="xetex")
 
         # rmarkdown::metadata holds document yaml metadata when knitting
-        if (is.character(rmarkdown::metadata$mainfont)) {
-            fontline <- paste0("\\setmainfont{", rmarkdown::metadata$mainfont,
-                               "}\n")
+
+        plotfont <- options$plot_font
+        if (!is.character(plotfont) || plotfont == "sansfont") {
+            plotfont <- rmarkdown::metadata$sansfont
+        } else if (plotfont == "mainfont") {
+            plotfont <- rmarkdown::metadata$mainfont
+        }
+
+        if (is.character(plotfont)) {
+            fontline <- paste0("\\setmainfont{", plotfont, "}\n")
         } else {
             fontline <- character()
         }
