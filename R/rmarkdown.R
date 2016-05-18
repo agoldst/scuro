52c58bee9b6b1e246fae28ff31200fded7121879
Andrew Goldstone <andrew.goldstone@gmail.com>
2016-05-18 12:43:41 -0400

    elsmd conversion: first finished draft
    scuro::make is invoked by skeleton/Makefile. Its work is to point the
    controlling package Makefile at the right files and directories, and to
    ensure that rmarkdown/knitr spews its files in the right places.
    
    skeleton.Rmd is required at the top level of the template directory by
    rmarkdown. rmarkdown::draft gives a warning without it, and RStudio
    won't list the template in the "New R Markdown" dialog. So I made a
    README there. The actual samples are the {notes,script}-sample.Rmd
    files.

--- R/rmarkdown.R
+++ R/rmarkdown.R
@@ -14,15 +14,16 @@
 #'
 #' @export
 scuro_md <- function (
-        variant="markdown",
-        preserve_yaml=TRUE,
         fig_width=4.5,
         fig_height=2.75,
         dev="tikz") {
 
     result <- rmarkdown::md_document(
-        variant=variant,
-        preserve_yaml=preserve_yaml,
+        # variant="markdown" and --standalone (always set by md_document)
+        # together entail the preservation of the YAML block *by pandoc*
+        variant="markdown",
+        # which means rmarkdown should not ALSO copy over the YAML block
+        preserve_yaml=FALSE,
         toc=FALSE,
         fig_width=fig_width,
         fig_height=fig_height,
@@ -49,6 +50,10 @@ scuro_md <- function (
     knitr_options$opts_chunk$size <- "footnotesize"
     knitr_options$opts_chunk$dev.args <- list(pointsize=9)
 
+    # put figures one level up, where they can be found by latex later on
+    # overrides rmarkdown's default choice of "<filename>_files"
+    knitr_options$opts_chunk$fig.path <- "../figure/"
+
     # custom package hook option: use dark_plot_theme everywhere
     knitr_options$opts_chunk$dark_theme <- TRUE
     if (dev == "tikz" && latex_engine == "xelatex") {
@@ -66,126 +71,45 @@ scuro_md <- function (
     result
 }
 
-#' Process R markdown into slides, notes, handouts, or all three
+#' Convert scuro markdown to PDF slides, notes, scripts, and handouts
 #'
-#' This function takes an R markdown file, knits it to ordinary markdown, and converts that markdown to one or all three of the supported PDF output types.
+#' Knitted markdown can be converted into the multiple forms that support a
+#' lecture with this function. It is just a wrapper for an invocation of
+#' \code{make}.
 #'
-#' It would be nice to make use all of the configurability of \code{rmarkdown::render} (and its RStudio integration), but that function expects to convert one input into one output. To create three outputs from one input, you have to define three output formats. This defeats knitr's cacheing and causes it to knit three times. If there are any serious computations in your R markdown, this is a drag. The alternative strategy adopted here is to knit once and do the rest with pandoc. This way if you render notes after having already rendered slides, your R chunks are not re-run.
+#' The possible targets are defined by the Makefile included in this package
+#' (\emph{not} the Makefile in the \code{lectures} template), which can be
+#' found at \code{system.file("elsmd/Makefile", package="scuro")}. 
 #'
-#' I recommend setting up Makefile rules invoking this with
-#' \code{R -e 'scuro::render("$<", "slides")'} and so on.
-#'
-#' The conversion is still done with rmarkdown's copy of pandoc.
-#'
-#' Some rmarkdown and pandoc options can be specified in the YAML metadata under the key \code{scuro:}.
-#
-#' highlight code syntax highlighting. I recommend "zenburn" (see
-#'   \code{\link[rmarkdown]{beamer_presentation}} for other choices).
-#'
-#' keep_tex whether to keep the intermediate LaTeX file. Often needed for
-#'   debugging errors.
-#'
-#' latex_engine I recommend xelatex (cf. above remarks about fonts).
-#'
-#' pandoc_args character vector of extra arguments to pandoc.
-#'
-#' @param input R markdown file name
-#' @param output_format one of \code{"slides", "notes", "handout"}
+#' @param target Which PDFs to make: for each file \code{notes/X.md} or
+#' \code{scripts/X.md}, the options are \code{slides/X.pdf} (slides),
+#' \code{lectures/X.pdf} (speaker notes), \code{handouts/X.pdf} (slide handout
+#' for the audience). The default, \code{all}, generates all three for each
+#' markdown file.
 #'
 #' @export
-render <- function (input,
-        output_format=c("slides", "notes", "handout")) {
-    output_format <- match.arg(output_format)
+make <- function (target="all") {
 
-    file_base <- gsub("\\.[^.]*$", "", input)
-    md_file <- paste0(file_base, ".md")
-
-    # merge in settings from YAML metadata
-    settings <- rmarkdown::metadata$scuro
-    md_params <- formals(scuro_md)
-    md_override <- intersect(names(settings), names(md_params))
-    md_params[md_override] <- settings[md_override]
-
-    rmarkdown::render(input, output_file=md_file,
-                      output_format=do.call(scuro_md, md_params))
-
-    tmpl <- system.file("pandoc/beamer.template", package="scuro")
-    overlay_filter <- system.file("python/overlay_filter",
+    makefile <- system.file(file.path("elsmd", "Makefile"), package="scuro")
+    overlay_filter <- system.file(file.path("elsmd", "overlay_filter"),
                                   package="scuro")
+    slides_template <- system.file(file.path("elsmd", "elsmd-slides.latex"),
+                                   package="scuro")
+    script_template <- system.file(file.path("elsmd", "beamerarticle.latex"),
+                                   package="scuro")
+    system2("make", c(
+        paste0("OVERLAY_FILTER=", overlay_filter),
+        paste0("SLIDES_TMPL=", slides_template),
+        paste0("SCRIPT_TMPL=", script_template), 
+        "NOTES=notes_md", "SCRIPTS=scripts_md",
+        "-f", makefile,
+        target
+    ))
 
-    pandoc_options <- list(
-        slide_level=1,
-        highlight="zenburn",
-        latex_engine="xelatex",
-        template=tmpl,
-        filter=overlay_filter,
-        include_in_header=hdr
-    )
-
-    if (!is.null(settings$pandoc)) {
-        pandoc_options[names(settings$pandoc)] <- settings$pandoc
-    }
-    keep_tex <- settings$keep_tex
-    if (is.null(keep_tex)) keep_tex <- FALSE
-
-    output_file <- paste0(file_base, "-", output_format, ".pdf")
-
-    hdr <- system.file(paste0("tex/preamble-", output_format, ".tex"),
-                       package="scuro")
-
-
-    md_temp <- FALSE
-    if (output_format == "handout") {
-        if (!is.null(settings) {
-            h_opt <- settings$handout
-            if (!is.null(h_opt) && !is.null(h_opt$invert_plots)
-                    && h_opt$invert_plots) {
-                pdir <- paste0(file_base, "_files")
-                invert_plots(file.path(pdir, "figure-markdown"),
-                             file.path(pdir, "figure-scuro-handout"))
-                # Now, just munge the rendered markdown to replace
-                # figure file names
-                # TODO would be a bit safer to walk the pandoc parse tree
-                md_str <- gsub(
-                    "(!\\[.*\\([^)]*)figure-markdown/",
-                    "\\1figure-scuro-handout/",
-                    readLines(md_file))
-                md_file <- tempfile(fileext=".md")
-                writeLines(md_str, md_file)
-                on.exit(unlink(md_file), add=TRUE)
-            }
-        }
-    }
-
-    if (output_format == "notes") {
-        pandoc_vars <- rmarkdown::pandoc_variable_arg("fontsize", "8pt")
-    } else if (output_format == "handout") {
-        pandoc_vars <- c("-V", "handout")
-    } else {
-        pandoc_vars <- NULL
-    }
-
-    pandoc_options <- paste0(
-        "--",
-        gsub("_", "-", names(pandoc_options)),
-        " ",
-        unlist(pandoc_options)
-    )
-
-    if (rmarkdown::scuro$keep_tex) {
-        tex_file <- sub("pdf$", "tex", output_file)
-        pandoc_convert(md_file, to="beamer", output=tex_file,
-            options=c(pandoc_options, pandoc_vars)
-        )
-        pandoc_covert(tex_file, output_file)
-    } else {
-        pandoc_convert(md_file, to="beamer", output=output_file,
-            options=c(pandoc_options, pandoc_vars)
-        )
-    }
 }
 
 
+# TODO invoke this in plot hook to generate file for handout
 invert_plots <- function (indir, outdir) {
     if (Sys.which("convert") == "") return()
 
@@ -207,18 +131,3 @@ invert_plots <- function (indir, outdir) {
         }
     }
 }
-
-#' Render all three presentation outputs
-#'
-#' A convenience function which simply calls \code{\link{render}} three times.
-#'
-#' @param input R markdown file.
-#'
-#' @export
-#'
-render_all <- function (input) {
-    render(input, "slides")
-    render(input, "notes")
-    render(input, "handout")
-}
-
