1aab5a294839931b811ad41c83773fd6f2e350d0
Andrew Goldstone <andrew.goldstone@gmail.com>
2016-05-21 11:49:52 -0400

    lecture templates basic test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(scuro)
+
+test_check("scuro")
---
+++ tests/testthat/test-lectures.R
@@ -0,0 +1,44 @@
+context("lectures template")
+
+tmp <- tempfile("scuro")
+dir.create(tmp)
+d <- file.path(tmp, "test")
+
+test_that("all expected files are present", {
+    rmarkdown::draft(d, "lectures", "scuro", edit=F)
+
+    expect_equal(list.files(d, recursive=T),
+        c("Makefile",
+          file.path("notes", "notes-sample.Rmd"),
+          file.path("scripts", "script-sample.Rmd"),
+          "test.Rmd"))
+    unlink(d, recursive=T)
+    expect_false(file.exists(d))
+})
+
+test_that("making slides does what we want", {
+    rmarkdown::draft(d, "lectures", "scuro", edit=F)
+    system2("make", c("-C", d, file.path("slides", "notes-sample.pdf")))
+    expect_equal(list.files(file.path(d, "slides")),
+                 c("notes-sample.pdf", "notes-sample.tex"))
+    unlink(d, recursive=T)
+    expect_false(file.exists(d))
+})
+
+test_that("default make does what we want", {
+    rmarkdown::draft(d, "lectures", "scuro", edit=F)
+    system2("make", c("-C", d))
+    expect_equal(list.files(file.path(d, "slides")),
+                 c(paste0("notes-", c("sample.pdf", "sample.tex")),
+                   paste0("script-", c("sample.pdf", "sample.tex"))))
+    expect_equal(list.files(file.path(d, "lectures")),
+                 c(paste0("notes-", c("sample.pdf", "sample.tex")),
+                   paste0("script-", c("sample.pdf", "sample.tex"))))
+    expect_equal(list.files(file.path(d, "handouts")),
+                 c(paste0("notes-", c("sample.pdf", "sample.tex")),
+                   paste0("script-", c("sample.pdf", "sample.tex"))))
+    unlink(d, recursive=T)
+    expect_false(file.exists(d))
+})
+
+unlink(tmp)
